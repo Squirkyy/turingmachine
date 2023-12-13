92881aab871c3f7e82e90bce713196ffc32cc344
Darius Vollmer <dariusv@outlook.de>
2023-12-13 16:54:37 +0100

    added script

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod turing_machine;
 use turing_machine::TuringMachine;
-
+// https://studip.uni-hannover.de/sendfile.php?type=0&file_id=9f53f0ebc4afad25285363991c41721d&file_name=Skript.pdf
 fn main() {
     println!("Hello, world!");
     let mut _tm = TuringMachine::new(Some('c'));
